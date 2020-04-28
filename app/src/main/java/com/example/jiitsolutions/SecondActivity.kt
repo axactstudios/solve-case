4140204a9a9ac39c5944cd0728057b511a226ee2
abhyu97 <57936464+abhyu97@users.noreply.github.com>
2020-04-28 09:45:12 +0530

    Splash Screen improved

--- app/src/main/java/com/example/jiitsolutions/SecondActivity.kt
+++ app/src/main/java/com/example/jiitsolutions/SecondActivity.kt
@@ -1,7 +1,12 @@
 package com.example.jiitsolutions
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.second_activity.*
 
 class SecondActivity : AppCompatActivity(){
 
@@ -9,5 +14,6 @@ class SecondActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.second_activity)
+
+        }
     }
-}
