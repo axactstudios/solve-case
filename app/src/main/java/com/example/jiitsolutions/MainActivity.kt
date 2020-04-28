3a85be495215e2b6960a6a2c2909d31d4295356b
scorchy <vkumarsaraswat@gmail.com>
2020-04-28 19:28:03 +0530

    splash screen added

--- app/src/main/java/com/example/jiitsolutions/MainActivity.kt
+++ app/src/main/java/com/example/jiitsolutions/MainActivity.kt
@@ -3,18 +3,40 @@ package com.example.jiitsolutions
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        logo.setOnClickListener {
-            Toast.makeText(this,"Button Clicked",Toast.LENGTH_SHORT).show()
-            val intent=Intent(this,SecondActivity::class.java)
-            startActivity(intent)
+        val background = object : Thread(){
+            override fun run() {
+                try{
+                    Thread.sleep(5000)
+
+                    val intent= Intent(baseContext,SecondActivity::class.java)
+                    startActivity(intent)
+                } catch(e : Exception){
+                    e.printStackTrace()
+                }
+            }
         }
+        background.start()
+                val anim = AnimationUtils.loadAnimation(this,R.anim.anim)
+        val anim2 = AnimationUtils.loadAnimation(this, R.anim.anim2)
+        logo.startAnimation(anim)
+        textView3.startAnimation(anim2)
+        textView2.startAnimation(anim2)
+
+
+
+
+
+
+
     }
 }
