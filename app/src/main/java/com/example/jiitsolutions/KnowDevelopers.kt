33dd1f14e5743d158e26c27ebc0ae36730ad74f3
scorchy <vkumarsaraswat@gmail.com>
2020-05-03 00:58:04 +0530

    google button resize. warnings removed

--- app/src/main/java/com/example/jiitsolutions/KnowDevelopers.kt
+++ app/src/main/java/com/example/jiitsolutions/KnowDevelopers.kt
@@ -1,12 +1,43 @@
 package com.example.jiitsolutions
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_know_developers.*
 
 class KnowDevelopers : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_know_developers)
+        btn_aviral.setOnClickListener {
+            val intent = Intent()
+            intent.action= Intent.ACTION_VIEW
+
+            intent.setData(Uri.parse("http://api.whatsapp.com/send?phone=+917060222315"))
+            startActivity(intent)
+        }
+        btn_shubh.setOnClickListener {
+            val intent = Intent()
+            intent.action= Intent.ACTION_VIEW
+
+            intent.setData(Uri.parse("http://api.whatsapp.com/send?phone=+919027553376"))
+            startActivity(intent)
+        }
+        btn_chitransh.setOnClickListener {
+            val intent = Intent()
+            intent.action= Intent.ACTION_VIEW
+
+            intent.setData(Uri.parse("http://api.whatsapp.com/send?phone=+918171907780"))
+            startActivity(intent)
+        }
+        btn_akash.setOnClickListener {
+            val intent = Intent()
+            intent.action= Intent.ACTION_VIEW
+
+            intent.setData(Uri.parse("http://api.whatsapp.com/send?phone=+917895833794"))
+            startActivity(intent)
+        }
     }
 }
